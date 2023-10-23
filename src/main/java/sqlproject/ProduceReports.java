82a0363bb8fdf3742b55e81f3d322806e0968f3e
s8m8 <148464317+s8m8@users.noreply.github.com>
2023-10-23 16:07:25 +0200

    Update ProduceReports.java

--- src/main/java/sqlproject/ProduceReports.java
+++ src/main/java/sqlproject/ProduceReports.java
@@ -6,6 +6,9 @@ import java.util.Properties;
 import java.util.Scanner;
 
 public class ProduceReports {
+     //This line declares a constant variable DB_URL that holds the URL for the MySQL database you're connecting to.
+    // It specifies the database location, port, and database name.
+
 
     private static final String DB_URL = "jdbc:mysql://localhost:3306/project2";
 
@@ -13,10 +16,14 @@ public class ProduceReports {
 
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
+            //These lines allow the user to input a customer ID. It uses a Scanner object to read input from the user.
             System.out.print("Enter customer ID: ");
             long customerId = scanner.nextLong();
 
-            Properties props = new Properties();
+           
+             //These lines load database configuration properties from a file named "config.properties."
+            // It includes the database username and password.
+             Properties props = new Properties();
             try (FileInputStream input = new FileInputStream("config.properties")) {
                 props.load(input);
             } catch (Exception e) {
@@ -25,15 +32,19 @@ public class ProduceReports {
             String DB_USER = props.getProperty("DB_USER");
             String DB_PASSWORD = props.getProperty("DB_PASSWORD");
 
-
+             //This code establishes a connection to the MySQL database using,
+            // the URL, username (DB_USER), and password (DB_PASSWORD) obtained from the properties file.
             try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
+                 //It calls a method named produceReports to generate reports based on the customer ID and the database connection.
                 produceReports(connection, customerId);
+                 //These lines catch and handle exceptions that might occur during the database connection or report generation.
+                // If there's an error, it prints the error details.
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
     }
-
+//This method is responsible for producing various reports based on the customer ID and the database connection.
     private static void produceReports(Connection connection, long customerId) throws SQLException {
 
         // Create a SQL statement
